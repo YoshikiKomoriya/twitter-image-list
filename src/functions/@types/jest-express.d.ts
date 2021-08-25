5013564bf03d1e602bd6e0a9f942b269763ad591
YoshikiKomoriya <yoshiki.komoriya@gmail.com>
2021-08-26 06:13:48 +0900

    既存のテストをライブラリ一本化&モックサーバーに対応するように修正

--- src/functions/@types/jest-express.d.ts
+++ src/functions/@types/jest-express.d.ts
@@ -2,6 +2,7 @@
  * ライブラリ'jest-express'の定義ファイルを拡張したもの
  */
 import { Express } from 'jest-express/lib/express'
+import Twitter from 'twitter-lite'
 
 declare module 'jest-express/lib/request' {
   /**
@@ -21,5 +22,18 @@ declare module 'jest-express/lib/request' {
       | 'CONNECT'
     headers?: any
     app?: Express
+  }
+
+  /**
+   * リクエスト情報内に格納するセッション情報
+   * express-sessionのSessionクラスをそのまま利用するのが望ましいが、以下の事由に対応するのに時間がかかりそうなため、暫定的に独自に定義している
+   * - コンストラクタがプライベートメソッドとして定義されているため、外部からインスタンスを生成できない
+   */
+  interface Session {
+    client?: Twitter
+  }
+
+  interface IRequest {
+    session: Session
   }
 }
