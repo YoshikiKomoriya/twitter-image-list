616144035a21b11ddc50c5d8ebc2b95782c700d4
YoshikiKomoriya <yoshiki.komoriya@gmail.com>
2021-09-14 21:44:10 +0900

    デフォルトレイアウトを追加

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,46 @@
 <template>
   <v-app>
-    <!-- 各ページのコンテンツ -->
+    <!-- ナビゲーションの実際の表示領域 -->
+    <navigation-drawer
+      :value="navigationDrawer"
+      @input="setNavigationDrawer"
+    ></navigation-drawer>
+
+    <header-component @input="setNavigationDrawer(true)"></header-component>
+
     <v-main>
-      <nuxt></nuxt>
+      <v-container>
+        <!-- 各ページのコンテンツ -->
+        <nuxt></nuxt>
+      </v-container>
     </v-main>
+
+    <footer-component></footer-component>
   </v-app>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import FooterComponent from '~/components/navigation/Footer.vue'
+import HeaderComponent from '~/components/navigation/Header.vue'
+import NavigationDrawer from '~/components/navigation/NavigationDrawer.vue'
+
+export default Vue.extend({
+  components: {
+    FooterComponent,
+    HeaderComponent,
+    NavigationDrawer,
+  },
+  data: () => ({
+    navigationDrawer: false,
+  }),
+  methods: {
+    /**
+     * ナビゲーションドロワーに関するモデルを設定する
+     */
+    setNavigationDrawer(value: boolean) {
+      this.navigationDrawer = value
+    },
+  },
+})
+</script>
