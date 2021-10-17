13eb208a0bfc67a8c791707936a0f6ab56ec33d3
YoshikiKomoriya <yoshiki.komoriya@gmail.com>
2021-10-17 09:50:10 +0900

    キーワード検索ページを追加

---
+++ src/pages/keyword/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row>
+    <v-col>
+      <h1 class="text-center">キーワード検索</h1>
+      <search-bar-keyword></search-bar-keyword>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import SearchBarKeyword from '~/components/form/SearchBarKeyword.vue'
+
+export default Vue.extend({
+  components: { SearchBarKeyword },
+})
+</script>
