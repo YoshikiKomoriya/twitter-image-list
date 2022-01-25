c9c88ffe8830d4962b6716ce32b77a322ea44746
YoshikiKomoriya <yoshiki.komoriya@gmail.com>
2022-01-26 01:24:58 +0900

    CI向けファイルの修正漏れ対応

---
+++ frontend/shims-vue.d.ts
@@ -0,0 +1,8 @@
+/**
+ * IDE向けに、vueファイルのimportを可能とする型設定を追加する
+ * 'importしようとしているvueファイルの型がわからない'という旨のエラーが出るため、'Vueクラスである'と明示的に設定している
+ */
+declare module '*.vue' {
+  import Vue from 'vue'
+  export default Vue
+}
