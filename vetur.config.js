e5cbfc484287671d28767a7f80388e91e53f3d29
YoshikiKomoriya <yoshiki.komoriya@gmail.com>
2022-01-26 00:23:55 +0900

    Veturがshims-vue.d.tsと競合していたため、後者を削除

--- vetur.config.js
+++ vetur.config.js
@@ -1,7 +1,11 @@
-export const projects = [
-  {
-    root: './nuxt',
-    package: './package.json',
-    tsconfig: './tsconfig.json',
-  },
-]
+// CommonJSの書式でないと設定を読みこまない様子なので、一時的に無視している
+// eslint-disable-next-line no-undef
+module.exports = {
+  projects: [
+    {
+      root: './frontend',
+      package: './package.json',
+      tsconfig: './tsconfig.json',
+    },
+  ],
+}
